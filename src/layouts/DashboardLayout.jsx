2ba5f3e9ecc14e84bf6cf164e376b3a2844e92a8
daksh963655 <daksh.kumar@aayaninfotech.com>
2025-09-15 10:58:21 +0530

    Update DashboardLayout.jsx

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -115,18 +115,13 @@ const DashboardLayout = () => {
           bottom: 0,
           scrollbarWidth: "thin",
           scrollbarGutter: "stable",
-          background: "rgba(87, 87, 247, 0.9)",
-          // background:
-          //   "linear-gradient(137deg, rgba(87, 87, 247, 1) 0%, rgba(253, 255, 182, 1) 100%)",
+          // background: "#5757F7",
+          background:
+            "linear-gradient(137deg, rgba(87, 87, 247, 1) 0%, rgba(73, 207, 174, 1) 49%, rgba(220, 163, 255, 1) 100%)",
         }}
       >
         <div className="demo-logo-vertical w-full mt-3">
-          <img
-            src={logo1}
-            alt="logo"
-            className="w-full"
-            style={{ maxHeight: "120px", objectFit: "contain" }}
-          />
+          <img src={logo1} alt="logo" className="w-full" style={{maxHeight: "120px" , objectFit:"contain"}} />
         </div>
         <Menu
           // theme="dark"
@@ -137,7 +132,7 @@ const DashboardLayout = () => {
             icon: item.icon,
             label: item.label,
           }))}
-          style={{ background: "inherit", marginTop: "20px" }}
+          style={{ background: "inherit" , marginTop: "20px"}}
           onClick={handleMenuClick}
         />
       </Sider>
